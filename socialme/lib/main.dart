5980bbc132ebd1c86439ebe1d9d575b953926677
balusubhanuprakash <balusubhanuprakash091@gmail.com>
2022-11-06 00:37:41 +0530

    Adding introduction slider
    I've used VS code to design these slides. This may cause some errors , please resolve in further use.

--- socialme/lib/main.dart
+++ socialme/lib/main.dart
@@ -1,27 +1,19 @@
 import 'package:flutter/material.dart';
-import 'loading.dart';
+
+import 'Introslider.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            body: Container(
-      width: double.infinity,
-      height: double.infinity,
-      decoration: BoxDecoration(
-          image: DecorationImage(
-        image: AssetImage("lib/images/Loading1.png"),
-        fit: BoxFit.cover,
-      )),
-      child: loading(),
-    )));
+    return const MaterialApp(
+      home: Introslider(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
